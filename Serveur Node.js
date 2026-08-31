79fdca6f9884739ebc00a75dc6d00d865cabc7d1
Mohamed <ms.dalichaouch@gmailcom>
2026-08-31 14:23:24 +0100

    Prototype WebSocket

---
+++ Serveur Node.js
@@ -0,0 +1,24 @@
+// npm install ws
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+
+let players = [];
+
+server.on('connection', socket => {
+  players.push(socket);
+  console.log("Nouveau joueur connecté !");
+
+  socket.on('message', msg => {
+    console.log("Reçu:", msg);
+    // Diffuser à tous les joueurs
+    players.forEach(p => {
+      if(p.readyState === WebSocket.OPEN) {
+        p.send(msg);
+      }
+    });
+  });
+
+  socket.on('close', () => {
+    players = players.filter(p => p !== socket);
+  });
+});
